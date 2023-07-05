737601207560b4cb1bdf07b46ffa1d98abc787c1
Aiden Black <blackaw1234@gmail.com>
2023-07-04 23:01:50 -0500

    following advanced item tutorial by kaupenjoe

--- src/main/java/net/aiden/aircraftmod/item/ModItems.java
+++ src/main/java/net/aiden/aircraftmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.aiden.aircraftmod.item;
 
 import net.aiden.aircraftmod.AircraftMod;
+import net.aiden.aircraftmod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,9 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.AIRCRAFT_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.AIRCRAFT_TAB)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.AIRCRAFT_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus)
     {
